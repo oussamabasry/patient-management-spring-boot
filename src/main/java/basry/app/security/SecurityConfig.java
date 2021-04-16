c428eee96da1a17003511fe3a37c7069e61a70ec
Oussama BASRY <58572674+oussamabasry@users.noreply.github.com>
2021-04-16 14:17:52 +0000

    JDBC

--- src/main/java/basry/app/security/SecurityConfig.java
+++ src/main/java/basry/app/security/SecurityConfig.java
@@ -1,5 +1,6 @@
 package basry.app.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -9,28 +10,45 @@ import org.springframework.security.crypto.bcrypt.BCrypt;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    private DataSource dataSource;
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         PasswordEncoder passwordEncoder = passwordEncoder();
+        System.out.println(passwordEncoder.encode("1234"));
+        // configuration JDBC Authentication
+        auth.jdbcAuthentication()
+                .dataSource(dataSource)
+                .usersByUsernameQuery("select username as principal, password as credentials, active from users where username = ?")
+                .authoritiesByUsernameQuery("select username as principal, role as role from users_roles where username = ?")
+                .passwordEncoder(passwordEncoder)
+                .rolePrefix("ROLE_");
+        // Memory Auth
+        /*
         auth.inMemoryAuthentication().withUser("user1").password(passwordEncoder.encode("1234")).roles("USER");
         auth.inMemoryAuthentication().withUser("user2").password(passwordEncoder.encode("1234")).roles("USER");
         auth.inMemoryAuthentication().withUser("admin").password(passwordEncoder.encode("1234")).roles("USER","ADMIN");
+         */
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.formLogin();
+        http.formLogin().loginPage("/login");
         //http.httpBasic();
         http.authorizeRequests().antMatchers("/save**/**", "/delete**/**", "/form**/**").hasRole("ADMIN");
         http.authorizeRequests().antMatchers("/patients").hasRole("USER");
-       // http.authorizeRequests().anyRequest().authenticated();
+        // http.authorizeRequests().anyRequest().authenticated();
         http.csrf();
         http.exceptionHandling().accessDeniedPage("/notAuthorized");
     }
+
     @Bean
-    public PasswordEncoder passwordEncoder(){
+    public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 }
--- src/main/java/basry/app/security/SecurityController.java
+++ src/main/java/basry/app/security/SecurityController.java
@@ -3,10 +3,24 @@ package basry.app.security;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 public class SecurityController {
     @GetMapping("/notAuthorized")
-    public  String error(){
+    public String error() {
         return "notAuthorized";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @GetMapping("/logout")
+    public String logout(HttpServletRequest request) throws ServletException {
+        request.logout();
+        return "redirect:/login";
     }
 }
